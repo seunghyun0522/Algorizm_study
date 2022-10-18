2f63843c4c311c437e2fcd3af1db089bae3ae698
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2022-10-18 14:51:02 +0900

    Update 실습2번.c

--- heap&heapSort/실습2번.c
+++ heap&heapSort/실습2번.c
@@ -1,94 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void swapElement(int*, int*);
-void upHeap(int*, int);
-void downHeap(int*, int, int);
-void insertItem(int*, int*, int);
-int deleteKey(int*, int*);
-void printHeap(int*, int N);
-void main() {
-	int H[100] = { 0 }, N = 0, key;
-	int* Element;
-	char func = '\0';
-
-	while (func != 'q') {
-
-		scanf("%c", &func);
-		switch (func) {
-
-
-			//삽입 함수 호출
-		case 'i':
-			scanf("%d", &key);
-			insertItem(H, &N, key);
-			break;
-		case 'd':
-			Element = deleteKey(H, &N);
-			if (Element >= 1)
-				printf("%d\n", Element);
-			break;
-		case 'p':
-			printHeap(H, N);
-			break;
-
-		}
-		getchar();
-	}
+int n=0;
+void insertItem(int *H, int k);
+void upHeap(int *H,int i);
+void swapElement(int *A, int *B);
+int removeMax(int *H);
+void rBuildHeap1(int *H,int i);
+void downHeap(int *H,int i);
+void printHeap(int *H);
+void rBuildHeap2(int *H);
+int main(){
+    int *H=NULL;
+    int key;
+    int num;
+    scanf("%d",&num);
+    H= (int*)malloc(sizeof(int)*(num+1));
+    for(int i=0;i<num;i++){
+        scanf("%d",&key);
+        insertItem(H,key);
+    }
+    rBuildHeap2(H);
+    printHeap(H);
+    return 0;
 }
-void swapElement(int* A, int* B) {
-	int tmp = *A;
-	*A = *B;
-	*B = tmp;
-}
-void upHeap(int* H, int i) {
-	if (i == 1)
-		return;
-	if (H[i / 2] >= H[i])
-		return;
-	swapElement(&H[i / 2], &H[i]);
-	upHeap(H, i / 2);
 
+void insertItem(int *H, int k){
+    n++;
+    H[n] = k;
+    //upHeap(H,n);
 }
-void downHeap(int* H, int N, int i) {
-	int bigger;
-	if (2 * i > N)
-		return;
-	bigger = 2 * i;
-	if (bigger + 1 <= N)
-		if (H[bigger] < H[bigger + 1])
-			bigger++;
-	if (H[bigger] <= H[i])
-		return;
-	swapElement(&H[bigger], &H[i]);
-	downHeap(H, N, bigger);
-}
-void insertItem(int* H, int* n, int key) {
-	if (*n >= 99) {
-		//삽입 실패 표현
-		return;
-	}
-	//마지막 노드 갱신 후 삽입
-	(*n)++;
-	H[*n] = key;
-	upHeap(H, *n);
 
-	//삽입 완료
-	printf("0\n");
+void upHeap(int *H,int i){
+    if(i<=1)return;
+    if(H[i/2]>=H[i])return;
+    swapElement(&H[i/2],&H[i]);
+    upHeap(H,i/2);
 }
-int deleteKey(int* H, int* n) {
-	int old;
-	if (*n == 0)
-		return -1;
-	old = H[1];
-	H[1] = H[*n];
-	H[*n] = 0;
-	(*n)--;
-	downHeap(H, *n, 1);
-	return old;
+
+void swapElement(int *A, int *B){
+    int temp =*A;
+    *A = *B;
+    *B = temp;
 }
-void printHeap(int* H, int N) {
-	for (int i = 1; i <= N; i++)
-		printf(" %d", H[i]);
-	printf("\n");
+
+/*
+int removeMax(int *H){
+    int deleteKey = H[1];
+    H[1] = H[n];
+    n--;
+    downHeap(H,1);
+    return deleteKey;
+}
+*/
+void downHeap(int *H,int i){
+    if(i<1)return;
+    int bigger= i*2;
+    if(bigger> n) return;
+    if(H[bigger] <= H[bigger+1]) bigger++;
+    
+    if(H[bigger]<H[i]) return;
+    else swapElement(&H[bigger],&H[i] );
+    downHeap(H,bigger);
+}
+
+//재귀 버전
+void rBuildHeap1(int *H,int i){
+    if(i<=n){
+        rBuildHeap1(H,2*i);
+        rBuildHeap1(H,2*i+1);
+        downHeap(H,i);
+    }
+}
+void rBuildHeap2(int *H){
+    for(int i=n/2;i>=1;i--) downHeap(H, i);
+}
+
+void printHeap(int *H){
+    for(int i=1;i<=n;i++)
+        printf("%d ",H[i]);
+    printf("\n");
 }
